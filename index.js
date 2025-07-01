082ae5ea7919c13072201be56ffe8b653a8fb460
Bala Vignesh <vbala8578@gmail.com>
2025-07-01 16:55:03 +0530

    Completed the working of login and signup

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import { loadImage } from "canvas"; // <-- Import from canvas
+import { loadImage } from "canvas";
+import dotenv from 'dotenv';
+import session from 'express-session';
+
+
+
 
 const app = express();
 const port = 3000;
-
+dotenv.config();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 function limitChars(text, maxChars) {
@@ -13,12 +18,20 @@ function limitChars(text, maxChars) {
   return text.slice(0, maxChars) + "...";
 }
 
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+
 let notes = [];
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "notes",
-  password: "1234",
+  password: process.env.db_password,
   port: 5432,
 });
 db.connect();
@@ -95,7 +108,7 @@ app.get("/book/:id", async (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
-app.get('/signup',(req,res)=>{
+app.get('/signup1',(req,res)=>{
   try{
     res.render('signup-step1.ejs');
   }
@@ -113,6 +126,33 @@ app.get('/login',(req,res)=>{
     res.status(500).send("Internal server error");
   }
 });
+app.get('/signup2', (req, res) => {
+  res.render('signup-step2.ejs');
+});
+app.post('/signup2', (req, res) => {
+  const signupData = req.session.signupData || {};
+  if(req.body.password===req.body.confirmPassword){
+  signupData.username = req.body.username;
+  signupData.password = req.body.password;
+  signupData.confirmPassword = req.body.confirmPassword;
+  req.session.signupData = null;
+  res.send("Signup complete! Data: " + JSON.stringify(signupData));
+  }
+  else{
+  res.redirect('/signup2');
+  }
+});
+
+app.post('/signup1',(req,res)=>{
+  req.session.signupData={
+    name: req.body.name,
+    age: req.body.age,
+    dob: req.body.dob,
+    sex: req.body.sex,
+  };
+  res.redirect('/signup2');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
